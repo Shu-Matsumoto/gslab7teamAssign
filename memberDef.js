b59717378c7ae77f4ad05aa912c00dbf1e73931e
Shu-Matsumoto <motomashu@gmail.com>
2022-06-01 09:41:05 +0900

    Start-Stop期間中のメンバーシャッフル機能を追加

--- memberDef.js
+++ memberDef.js
@@ -1,9 +1,41 @@
+// 参加者の名前
+const MemberNameAbematsu = "あべまつ";
+const MemberNameOkawauchi = "おおかわうち";
+const MemberNameTamaru = "たまる";
+const MemberNameNakashima = "なかしま";
+const MemberNameNasu = "なす";
+const MemberNameMatsumoto = "まつもと";
+const MemberNameMishima= "みしま";
+
 // メンバーリスト
-export const MemberList = [
-  "あべまつ",
-  "おおかわうち",
-  "たまる",
-  "なかしま",
-  "なす",
-  "まつもと",
-  "みしま"];
+// export const MemberList = [
+//   MemberNameAbematsu,
+//   MemberNameOkawauchi,
+//   MemberNameTamaru,
+//   MemberNameNakashima,
+//   MemberNameNasu,
+//   MemberNameMatsumoto,
+//   MemberNameMishima
+// ];
+
+// メンバーリスト
+export const MemberList = new Map([
+  [MemberNameAbematsu,  0],
+  [MemberNameOkawauchi, 1],
+  [MemberNameTamaru,    2],
+  [MemberNameNakashima, 3],
+  [MemberNameNasu,      4],
+  [MemberNameMatsumoto, 5],
+  [MemberNameMishima,   6],
+]);
+
+// ミスマッチメンバーリスト
+export const MissmatchMemberList = new Map([
+  [MemberNameAbematsu,  [MemberNameNasu, MemberNameMishima]],
+  [MemberNameOkawauchi, [MemberNameMatsumoto,]],
+  [MemberNameTamaru,    [MemberNameNakashima,]],
+  [MemberNameNakashima, [MemberNameTamaru,]],
+  [MemberNameNasu,      [MemberNameAbematsu, MemberNameMishima]],
+  [MemberNameMatsumoto, [MemberNameOkawauchi,]],
+  [MemberNameMishima,   [MemberNameAbematsu, MemberNameNasu]],
+]);
